05f7584b4af97d0452c714c54b2e430962762e26
MJunWen <mokjunwen@gmail.com>
2020-11-02 17:09:24 +0800

    page number

--- insertintotables.sql
+++ insertintotables.sql
@@ -6,13 +6,45 @@ insert into products
 values (
     1,
     "img/keyboardone.JPG",
-    "Lorem ipsum dolor sit amet Keyboard.",
+    "Lorem ipsum dolor sit amet Keyboard1.",
     1.23,
     "Keyboard",
     10
   ),
   (
     2,
+    "img/keyboardtwo.JPG",
+    "Lorem ipsum dolor sit amet Keyboard2.",
+    11.12,
+    "Keyboard",
+    9
+  ),
+  (
+    3,
+    "img/keyboardthree.JPG",
+    "Lorem ipsum dolor sit amet Keyboard3.",
+    11.13,
+    "Keyboard",
+    9
+  ),
+  (
+    4,
+    "img/keyboardfour.JPG",
+    "Lorem ipsum dolor sit amet Keyboard4.",
+    11.14,
+    "keyboard",
+    9
+  ),
+  (
+    5,
+    "img/keyboardfive.JPG",
+    "Lorem ipsum dolor sit amet Keyboard5.",
+    11.15,
+    "Keyboard",
+    9
+  ),
+  (
+    6,
     "img/mouseone.JPG",
     "Lorem ipsum dolor sit amet Mouse.",
     4.56,
@@ -21,7 +53,7 @@ values (
   ),
   
   (
-    3,
+    7,
     "img/usbone.JPG",
     "Lorem ipsum dolor sit amet USB.",
     7.89,
@@ -29,7 +61,7 @@ values (
     8
   ),
   (
-    4,
+    8,
     "img/mobilephoneone.JPG",
     "Lorem ipsum dolor sit amet phone.",
     4.98,
--- products.php
+++ products.php
@@ -8,6 +8,9 @@ if (isset($_GET['buy'])) {
     header('location: ' . $_SERVER['PHP_SELF'] . '?' . SID);
     exit();
 }
+if (isset($_GET['product'])) { //put category into session so can change page while keeping the same category
+    $_SESSION['category'] = $_GET['product'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -27,8 +30,8 @@ if (isset($_GET['buy'])) {
 
     //If click on category, filter out stuff based on category. Else show default 3 products
 
-    if (isset($_GET['product'])) {
-        $query = "SELECT * from products where category= '" . $_GET['product'] . "'";
+    if (isset($_SESSION['category'])) {
+        $query = "SELECT * from products where category= '" . $_SESSION['category'] . "'";
 
         $result = $db->query($query);
 
@@ -69,6 +72,30 @@ if (isset($_GET['buy'])) {
             $product_quantity[] = $row['quantity'];
         }
     }
+                $firstproductnumber = 0;
+                $secondproductnumber = 1;
+                $thirdproductnumber = 2;
+
+    if (isset($_GET['page'])) {
+        switch ($_GET['page']) {
+            case 1:
+                $firstproductnumber = 0;
+                $secondproductnumber = 1;
+                $thirdproductnumber = 2;
+                break;
+            case 2:
+                $firstproductnumber = 3;
+                $secondproductnumber = 4;
+                $thirdproductnumber = 5;
+                break;
+            case 3:
+                $firstproductnumber = 6;
+                $secondproductnumber = 7;
+                $thirdproductnumber = 8;
+                break;
+            default:
+        }
+    }
     ?>
 
 
@@ -96,81 +123,89 @@ if (isset($_GET['buy'])) {
             <!--fill from left to right, down 1 row, left to right repeat-->
 
             <div class="productimg">
-                <?php if (isset($product_src[0])) {
-                    echo "<img src='" . $product_src[0] . "'>";
+                <?php if (isset($product_src[$firstproductnumber])) {
+                    echo "<img src='" . $product_src[$firstproductnumber] . "'>";
                 } ?>
             </div>
             <div class="productimg">
-                <?php if (isset($product_src[1])) {
-                    echo "<img src='" . $product_src[1] . "'>";
+                <?php if (isset($product_src[$secondproductnumber])) {
+                    echo "<img src='" . $product_src[$secondproductnumber] . "'>";
                 } ?>
             </div>
             <div class="productimg">
-                <?php if (isset($product_src[2])) {
-                    echo "<img src='" . $product_src[2] . "'>";
+                <?php if (isset($product_src[$thirdproductnumber])) {
+                    echo "<img src='" . $product_src[$thirdproductnumber] . "'>";
                 } ?>
             </div>
 
             <div class="productname">
-                <?php if (isset($product_name[0])) {
-                    echo "$product_name[0]";
+                <?php if (isset($product_name[$firstproductnumber])) {
+                    echo "$product_name[$firstproductnumber]";
                 } ?>
             </div>
             <div class="productname">
-                <?php if (isset($product_name[1])) {
-                    echo "$product_name[1]";
+                <?php if (isset($product_name[$secondproductnumber])) {
+                    echo "$product_name[$secondproductnumber]";
                 } ?>
             </div>
             <div class="productname">
-                <?php if (isset($product_name[2])) {
-                    echo "$product_name[2]";
+                <?php if (isset($product_name[$thirdproductnumber])) {
+                    echo "$product_name[$thirdproductnumber]";
                 } ?>
             </div>
 
             <div class="productprice">
-                <?php if (isset($product_price[0])) {
-                    echo "$$product_price[0]";
+                <?php if (isset($product_price[$firstproductnumber])) {
+                    echo "$$product_price[$firstproductnumber]";
                 } ?>
             </div>
             <div class="productprice">
-                <?php if (isset($product_price[1])) {
-                    echo "$$product_price[1]";
+                <?php if (isset($product_price[$secondproductnumber])) {
+                    echo "$$product_price[$secondproductnumber]";
                 } ?>
             </div>
             <div class="productprice">
-                <?php if (isset($product_price[2])) {
-                    echo "$$product_price[2]";
+                <?php if (isset($product_price[$thirdproductnumber])) {
+                    echo "$$product_price[$thirdproductnumber]";
                 } ?>
             </div>
 
             <div class="addtocart">
-                <?php if (isset($product_id[0])) {
-                    if ($product_quantity[0] <= 0) {
+                <?php if (isset($product_id[$firstproductnumber])) {
+                    if ($product_quantity[$firstproductnumber] <= 0) {
                         echo "<a>Sold out</a>";
                     } else {
-                        echo " <a href='" . $_SERVER['PHP_SELF'] . '?buy=' . $product_id[0] . "'>Buy</a>";
+                        echo " <a href='" . $_SERVER['PHP_SELF'] . '?buy=' . $product_id[$firstproductnumber] . "'>Buy</a>";
                     }
                 } ?>
             </div>
 
             <div class="addtocart">
-            <?php if (isset($product_id[1])) {
-                    if ($product_quantity[1] <= 0) {
+                <?php if (isset($product_id[$secondproductnumber])) {
+                    if ($product_quantity[$secondproductnumber] <= 0) {
                         echo "<a>Sold out</a>";
                     } else {
-                        echo " <a href='" . $_SERVER['PHP_SELF'] . '?buy=' . $product_id[1] . "'>Buy</a>";
+                        echo " <a href='" . $_SERVER['PHP_SELF'] . '?buy=' . $product_id[$secondproductnumber] . "'>Buy</a>";
                     }
                 } ?>
             </div>
 
             <div class="addtocart">
-            <?php if (isset($product_id[2])) {
-                    if ($product_quantity[2] <= 0) {
+                <?php if (isset($product_id[$thirdproductnumber])) {
+                    if ($product_quantity[$thirdproductnumber] <= 0) {
                         echo "<a>Sold out</a>";
                     } else {
-                        echo " <a href='" . $_SERVER['PHP_SELF'] . '?buy=' . $product_id[2] . "'>Buy</a>";
+                        echo " <a href='" . $_SERVER['PHP_SELF'] . '?buy=' . $product_id[$thirdproductnumber] . "'>Buy</a>";
                     }
                 } ?>
+            </div>
+
+            <div class="pagenumber">
+                <?php
+                 echo " <a href='" . $_SERVER['PHP_SELF'] . '?page=' . 1 . "'>1</a>"; 
+                 echo " <a href='" . $_SERVER['PHP_SELF'] . '?page=' . 2 . "'>2</a>";
+                 echo " <a href='" . $_SERVER['PHP_SELF'] . '?page=' . 3 . "'>3</a>"; 
+                ?>
             </div>
 
         </div>
